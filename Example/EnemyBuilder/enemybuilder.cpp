3a8808e3354a75e49c6de5d500c8fc12a3920951
T_Sumisaki <mail.sumisaki@gmail.com>
2017-11-04 10:05:26 +0900

    add sample EnemyBuilder

---
+++ Example/EnemyBuilder/enemybuilder.cpp
@@ -0,0 +1,91 @@
+﻿#pragma once
+
+#include <iostream>
+#include "enemybuilder.h"
+
+
+
+class EasyEnemyBuilder : public IEnemyBuilder {
+private:
+    const float m_rate = 0.5;
+
+public:
+    void setHP(AEnemy* _enemy, float _hp) override {
+        _enemy->setHP(_hp * m_rate);
+    }
+    void setAtk(AEnemy* _enemy, float _atk) override {
+        _enemy->setAtk(_atk * m_rate);
+    }
+    void setDef(AEnemy* _enemy, float _def) override {
+        _enemy->setDef(_def * m_rate);
+    }
+};
+
+class NormalEnemyBuilder : public IEnemyBuilder {
+private:
+    const float m_rate = 1.0;
+
+public:
+    void setHP(AEnemy* _enemy, float _hp) override {
+        _enemy->setHP(_hp * m_rate);
+    }
+    void setAtk(AEnemy* _enemy, float _atk) override {
+        _enemy->setAtk(_atk * m_rate);
+    }
+    void setDef(AEnemy* _enemy, float _def) override {
+        _enemy->setDef(_def * m_rate);
+    }
+};
+
+class EnemyDirector : public ADirector {
+
+private:
+    AEnemy* createInstance() override {
+        return new AEnemy();
+    }
+};
+
+
+int main() {
+
+    EnemyDirector director;
+
+    IEnemyBuilder* easy = new EasyEnemyBuilder();
+    IEnemyBuilder* normal = new NormalEnemyBuilder();
+
+    AEnemy* enemy;
+
+    // Easy版
+    std::cout << "-- EASY --" << std::endl;
+
+    director.setBuilder(easy);
+    enemy = director.construct(50, 10, 7);
+
+    EnemyDocument::PrintStatus(enemy);
+
+    delete enemy;
+
+
+    // Normal版
+    std::cout << "-- NORMAL --" << std::endl;
+
+    director.setBuilder(normal);
+    enemy = director.construct(50, 10, 7);
+
+    EnemyDocument::PrintStatus(enemy);
+
+    delete enemy;
+
+
+    delete easy;
+    delete normal;
+
+
+    // ストッパー（Enterを押すと続く）
+	std::cout << "Press ENTER KEY to continue..." << std::endl;
+    getchar();
+
+
+    return 0;
+
+}
---
+++ Example/EnemyBuilder/enemybuilder.h
@@ -0,0 +1,74 @@
+﻿#pragma once
+
+#include <iostream>
+
+class AEnemy {
+protected:
+    float m_hp;
+    float m_atk;
+    float m_def;
+
+public:
+    float getHP() {
+        return m_hp;
+    }
+    void setHP(float _hp) {
+        m_hp = _hp;
+    }
+    float getAtk() {
+        return m_atk;
+    }
+    void setAtk(float _atk) {
+        m_atk = _atk;
+    }
+    float getDef() {
+        return m_def;
+    }
+    void setDef(float _def) {
+        m_def = _def;
+    }
+};
+
+
+class IEnemyBuilder {
+public:
+    virtual void setHP(AEnemy* _enemy, float _hp) = 0;
+    virtual void setAtk(AEnemy* _enemy, float _atk) = 0;
+    virtual void setDef(AEnemy* _enemy, float _def) = 0;
+};
+
+class ADirector {
+private:
+    IEnemyBuilder* m_builder;
+
+    virtual AEnemy* createInstance() = 0;
+
+public:
+    void setBuilder(IEnemyBuilder* _builder) {
+        m_builder = _builder;
+    }
+    AEnemy* construct(float _hp, float _atk, float _def) {
+
+        AEnemy* _enemy = createInstance();
+        if (m_builder) {
+            m_builder->setHP(_enemy, _hp);
+            m_builder->setAtk(_enemy, _atk);
+            m_builder->setDef(_enemy, _def);
+        }
+
+        return _enemy;
+    }
+
+};
+
+
+class EnemyDocument {
+public:
+    static void PrintStatus(AEnemy* _enemy) {
+
+        std::cout << "HP: " << _enemy->getHP() << std::endl;
+        std::cout << "ATK: " << _enemy->getAtk() << std::endl;
+        std::cout << "DEF: " << _enemy->getDef() << std::endl;
+
+    }
+};
